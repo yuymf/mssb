7484ad86c4940736fa978c67a6b9b47c4da156f7
yuymf <1352948945@qq.com>
2022-04-09 18:15:26 +0800

    1

---
+++ Q1.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/21 20:06
+# @Author  : gan
+#Q1 PCA+LDA
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from myscore import score
+# 读入数据并且分析数据
+
+
+def show(s):
+    print(s)
+    print(np.mean(s))
+
+file_location = "作业数据_2021合成.xls"
+data = pd.read_excel(file_location)
+data.dropna(axis=0, how='any', subset=['肺活量'], inplace=True)
+kf = KFold(n_splits = 5,shuffle = True,random_state = None)
+se_all=[]
+sp_all=[]
+acc_all=[]
+auc_all=[]
+for train_index, test_index in kf.split(data):
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    x_train =np.array([data["肺活量"].iloc[train_index],data["50米成绩"].iloc[train_index],
+              data["身高"].iloc[train_index],data["体重"].iloc[train_index],data["鞋码"].iloc[train_index]])
+    x_test =np.array([data["肺活量"].iloc[test_index],data["50米成绩"].iloc[test_index],
+             data["身高"].iloc[test_index],data["体重"].iloc[test_index],data["鞋码"].iloc[test_index]])
+    y_train, y_test =np.array(data["性别男1女0"].iloc[train_index]),np.array(data["性别男1女0"].iloc[test_index])
+    x_train=np.transpose(x_train);x_test=np.transpose(x_test)
+    pca = PCA(n_components=3, svd_solver='randomized', whiten=True).fit(x_train)
+    x_train_pca = pca.transform(x_train)
+    x_test_pca = pca.transform(x_test)
+    lda = LDA(n_components=1)
+    lda.fit(x_train_pca, y_train)
+    lda_pred=lda.predict(x_test_pca)
+    se,sp,acc,auc =score(lda_pred,y_test)
+    se_all.append(se);sp_all.append(sp);acc_all.append(acc);auc_all.append(auc)
+
+show(se_all);show(sp_all);show(acc_all);show(auc_all)
---
+++ Q2.py
@@ -0,0 +1,99 @@
+# -- coding: utf-8 --
+# @Author : wang
+# @File : Tree.py
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn import preprocessing
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import graphviz
+from myscore import score
+
+def pre_data():
+    """
+    考虑到不同特征属于不同的量级，需要进行标准化处理
+    :return: 处理后的特征数据和标签数据
+    """
+    file_loc = "作业数据_2021合成.xls"
+    file_data = pd.read_excel(file_loc)
+    data_color = file_data[["喜欢颜色"]]
+    ordinal_encoder = OrdinalEncoder()
+    color_encoder = ordinal_encoder.fit_transform(data_color)
+    print(color_encoder, data_color)
+    data_feature = np.hstack((color_encoder, pd.DataFrame(file_data, columns=['喜欢运动', '喜欢文学']).values))
+    scaler = preprocessing.StandardScaler().fit(data_feature)
+    data_feature = scaler.transform(data_feature)
+    data_label = file_data.性别男1女0.values
+    return  data_feature, data_label
+
+
+def split_data(data_feature, data_label):
+    """
+    分层采样，训练集为0.8，测试集为0.2
+    :param data_label: 标签数据
+    :param data_feature: 特征数据集
+    :return: 测试集和训练集
+    """
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(data_feature, data_label):
+        data_train, data_test = data_feature[train_index], data_feature[test_index]
+        label_train, label_test = data_label[train_index], data_label[test_index]
+    scaler = preprocessing.StandardScaler().fit(data_train)
+    data_train=scaler.transform(data_train)
+    data_test =scaler.transform(data_test)
+    return data_train, data_test, label_train, label_test
+
+
+if __name__ == '__main__':
+    data_feature, data_label = pre_data()
+    # print(data_feature, data_label)
+    data_train, data_test, label_train, label_test = split_data(data_feature, data_label)
+
+    # 绘制树模型
+    clf = DecisionTreeClassifier(criterion="entropy", random_state=30, splitter='random',max_depth=7)
+    clf = clf.fit(data_train, label_train)
+    score_train = clf.score(data_train, label_train)
+    score = clf.score(data_test, label_test)
+    # 训练集上的准确率为0.85，测试集上的准确率为0.7571428571428571
+    print(score_train, score)
+
+    tree.export_graphviz(clf)
+    dot_data = tree.export_graphviz(clf, out_file=None)
+    graphviz.Source(dot_data)
+
+    # 给图形增加标签和颜色
+    feature_name = ['color', 'sport', 'literature']
+    dot_data = tree.export_graphviz(clf, out_file=None, feature_names=['color', 'sport', 'literature'],
+                                    class_names=['girl', 'boy'], filled=True, rounded=True,
+                                    special_characters=True)
+    graphviz.Source(dot_data)
+
+    # 利用render方法生成图形
+    graph = graphviz.Source(dot_data)
+    graph.render("classify")
+
+    print(clf.feature_importances_)
+    print([*zip(feature_name, clf.feature_importances_)])
+
+    # 最优剪枝参数
+    test = []
+    for i in range(10):
+        clf = tree.DecisionTreeClassifier(criterion="entropy", random_state=30, splitter='random', max_depth=i + 1)
+        clf = clf.fit(data_train, label_train)
+        score = clf.score(data_test, label_test)
+        test.append(score)
+    # 设置绘图风格
+    plt.style.use('ggplot')
+    # 处理中文乱码
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    # 处理坐标轴负号
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.plot(range(1, 11), test, color="red", label="max_depth")
+    plt.xlabel('depth')
+    plt.ylabel('ACC')
+    plt.legend()
+    plt.show()
+    print(score_train, score)
